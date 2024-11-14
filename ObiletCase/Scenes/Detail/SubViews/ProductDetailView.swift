0c6763fc25a6d071a0776a96a3b30301336bce4c
Şehriban Yıldırım <sehribanyildirim051@gmail.com>
2024-11-14 17:37:06 +0300

    ProductDetailView edits completed

--- ObiletCase/Scenes/Detail/SubViews/ProductDetailView.swift
+++ ObiletCase/Scenes/Detail/SubViews/ProductDetailView.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ProductDetailView: UIView {
-
+    
+    //MARK: - Properties
     lazy var imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
@@ -18,7 +19,7 @@ class ProductDetailView: UIView {
     
     lazy var titleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
         label.textAlignment = .left
         label.numberOfLines = 0
         label.textColor = UIColor.appBlack
@@ -27,14 +28,90 @@ class ProductDetailView: UIView {
     
     lazy var descriptionLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
         label.textAlignment  = .left
-        label.textColor = .appBlack
+        label.textColor = .appGray
         label.numberOfLines = 0
         return label
     }()
     
+    lazy var priceStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    lazy var rateStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    lazy var personStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    lazy var viewStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 100
+        stackView.alignment = .center
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+    
+    
+    lazy var priceImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "dollar_icon")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    lazy var rateImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "star_circle")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    lazy var personImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "user_icon")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        label.textAlignment = .left
+        label.numberOfLines = 3
+        label.textColor = UIColor.appBlack
+        return label
+    }()
+    
+    lazy var rateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+        label.textAlignment = .left
+        label.numberOfLines = 3
+        label.textColor = UIColor.appBlack
+        return label
+    }()
+    
+    lazy var personLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
         label.textAlignment = .left
@@ -59,31 +136,26 @@ extension ProductDetailView{
     private func addSubViews(){
         addImageView()
         addTitleLabel()
-        addPriceLabel()
         addDescriptionLabel()
+        addPriceStackView()
+        addRateStackView()
+        addPersonStackView()
+        addViewStackView()
     }
     
     private func addImageView(){
         addSubview(imageView)
         imageView.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(10)
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(20)
             make.leading.trailing.equalToSuperview().inset(20)
-            make.width.height.equalTo(200)
+            make.width.height.equalTo(250)
         }
     }
     
     private func addTitleLabel(){
         addSubview(titleLabel)
         titleLabel.snp.makeConstraints { make in
-            make.top.equalTo(imageView.snp.bottom).offset(10)
-            make.leading.trailing.equalToSuperview().inset(20)
-        }
-    }
-    
-    private func addPriceLabel(){
-        addSubview(priceLabel)
-        priceLabel.snp.makeConstraints { make in
-            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.top.equalTo(imageView.snp.bottom).offset(15)
             make.leading.trailing.equalToSuperview().inset(20)
         }
     }
@@ -91,7 +163,37 @@ extension ProductDetailView{
     private func addDescriptionLabel(){
         addSubview(descriptionLabel)
         descriptionLabel.snp.makeConstraints { make in
-            make.top.equalTo(priceLabel.snp.bottom).offset(10)
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview().inset(20)
+        }
+    }
+    
+    private func addPriceStackView() {
+        addSubview(priceStackView)
+        priceStackView.addArrangedSubview(priceImageView)
+        priceStackView.addArrangedSubview(priceLabel)
+    }
+    
+    private func addRateStackView() {
+        addSubview(rateStackView)
+        rateStackView.addArrangedSubview(rateImageView)
+        rateStackView.addArrangedSubview(rateLabel)
+    }
+    
+    private func addPersonStackView() {
+        addSubview(personStackView)
+        personStackView.addArrangedSubview(personImageView)
+        personStackView.addArrangedSubview(personLabel)
+    }
+    
+    
+    private func addViewStackView(){
+        addSubview(viewStackView)
+        viewStackView.addArrangedSubview(priceStackView)
+        viewStackView.addArrangedSubview(rateStackView)
+        viewStackView.addArrangedSubview(personStackView)
+        viewStackView.snp.makeConstraints { make in
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(25)
             make.leading.trailing.equalToSuperview().inset(20)
         }
     }
